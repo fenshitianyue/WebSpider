ef057e2a4e2d16ef443a0eceec8ccee160fb1a90
fenshitianyue <abzanda@163.com>
2019-01-19 14:52:15 +0800

    A program for downloading photos from web page

---
+++ image_install.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import urllib2
+from bs4 import BeautifulSoup
+
+
+
+
+def init_url_pool():
+    f = open('main_url', 'r')
+
+
+def close_url_pool(f):
+    f.close()
+
+if __name__ == '__main__':
+    init_url_pool()
+    pass
