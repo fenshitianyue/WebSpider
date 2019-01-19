f437853edabf8ed036fe712a1215f39ae71eab51
fenshitianyue <abzanda@163.com>
2019-01-19 23:14:31 +0800

    The basic framework is complete

--- image_install.py
+++ image_install.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python
-#coding:utf-8
+# coding:utf-8
 
 import urllib2
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-
-
+# 定义一个全局列表用于保存url pool
+url_list = []
 
 def init_url_pool():
     f = open('main_url', 'r')
-
-
-def close_url_pool(f):
+# 将本地文件中的url依次添加到url_list中并去掉每个url末尾的换行符
+    for line in f.readlines():
+        url_list.append(line.strip())
+# 上面的两行代码也可以用列表推导语法(如下一行代码)
+# url_list = [line.strip() for line in f.readlines()]
     f.close()
+
+def test_url_pool():
+    fp = open('test_url', 'w+')
+    for line in url_list:
+        fp.write(line + '\n')
+    fp.close()
+
+def test_update_for_url():
+    f1 = open('image1.jpg', 'w+')
+    # test code
+    # print 'ready open url' + ' : ' + 'i.imgur.com/5HUw3J0.jpg?1'
+    # res_1 = urllib2.urlopen('http://i.imgur.com/5HUw3J0.jpg?1')
+    # print 'uropen ok'
+    for i in len(url_list):
+        print 'ready open url' + ' : ' + url_list[i]
+        try:
+            res_1 = urllib2.urlopen(url_list[i])
+        except BaseException:
+            print 'Crawl failure'
+            continue
+        else:
+            f1.write(res_1.read())
+    f1.close()
 
 if __name__ == '__main__':
     init_url_pool()
-    pass
+    test_update_for_url()
